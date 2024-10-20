efe8ca1381071251f17a842064ae92cf5dcd453e
Marc Luzuriaga <Mjluzuriaga109@gmail.com>
2024-10-19 22:32:17 -0700

    Debugging Fork Algorithm

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -53,8 +53,9 @@ int main(int argc, char *argv[])
 		while (argv[i] != NULL){
 			//Fork the process
 			int rc = fork();
+			//First Program to Run
+			int j = i-1;
 			if (i==1){
-				j = i-1
 				//CASE 1: Fork failed; exit
 				if (rc < 0){
 					fprintf(stderr, "fork failed\n");
@@ -71,26 +72,21 @@ int main(int argc, char *argv[])
 						close(pipefd[k][0]);
 						close(pipefd[k][1]);
 					}
-					
-					
-				}
-				//CASE 3: Parent
-				else{
-					//Close the read end of the parent
-					close(pipefd[0]);
-					//Copy the stdout file descriptor into the write end of the pipe's file descriptor
-					dup2(STDOUT_FILENO, pipefd[1]);
-					//Close the write end once finished
-					close(pipefd[1]);
-					if (execlp(argv[0], argv[0], NULL)){
+					//Run the corresponding program
+					if (execlp(argv[i], argv[i], NULL)){
 						perror("Error Forking Process");
 						exit(1);
 					}
+				}
+				//CASE 3: Parent
+				else{
+					//Close the write end once finished
+					close(pipefd[j][1]);
 					int rc_wait = wait(NULL);
 				}
 			}
+			//Rest of the programs to run
 			else if(i==argc-1){
-				j = i-1
 				//CASE 1: Fork failed; exit
 				if (rc < 0){
 					fprintf(stderr, "fork failed\n");
@@ -106,31 +102,53 @@ int main(int argc, char *argv[])
 					for (int k = 0; k < num_pipes; k++){
 						close(pipefd[k][0]);
 					}
-				}
-				//CASE 3: Parent
-				else{
-					//Close the write end of the parent
-					close(pipefd[j][]);
-					//Copy the stdout file descriptor into the write end of the pipe's file descriptor
-					dup2(STDOUT_FILENO, pipefd[1]);
-					if (execlp(argv[0], argv[0], NULL)){
+					//Run the corresponding program
+					if (execlp(argv[i], argv[i], NULL)){
 						perror("Error Forking Process");
 						exit(1);
 					}
-					int rc_wait = wait(NULL);
 
+				}
+				//CASE 3: Parent
+				else{
 					//Close the rest of the pipe file descriptors
 					//HYPOTHESIS: Close all file descriptors that are [k][0]
 					for (int k = 0; k < num_pipes; k++){
 						close(pipefd[k][0]);
 					}
-
+					int rc_wait = wait(NULL);
 				}
 			}
+			//Last program to run
 			else{
-			
+				//CASE 1: Fork failed; exit
+				if (rc < 0){
+					fprintf(stderr, "fork failed\n");
+					exit(1);
+				}
+				//CASE 2: Child process from fork
+				else if(rc==0){
+					//Copy the read end of the child's pipe file descriptor into stdin file descriptor.
+					dup2(pipefd[j-1][0], STDIN_FILENO);
+					//Copy the stdout file descriptor to the write end of the child's pipe file descriptor.
+					dup2(STDOUT_FILENO, pipefd[j][1]);
+					//Close the pipe file descriptors
+					close(pipefd[j-1][0]);
+					//Run the corresponding program
+					if (execlp(argv[i], argv[i], NULL)){
+						perror("Error Forking Process");
+						exit(1);
+					}
+				}
+				//CASE 3: Parent
+				else{
+					//Close the pipe file descriptors
+					close(pipefd[j-1][0]);
+					int rc_wait = wait(NULL);
+				}
+
 			}
-			
+		i++;
 		}
 		
 	}
